8a4ad4d21811cb41d3b70fddbbc2fc08a60dcd1e
yukuda <leeyookoo@naver.com>
2021-07-14 22:05:47 +0900

    Spring

---
+++ core/src/test/java/hello/core/CoreApplicationTests.java
@@ -0,0 +1,19 @@
+package hello.core;
+
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+
+@SpringBootTest
+class CoreApplicationTests {
+
+
+	@Test
+	void contextLoads() {
+	}
+
+}
